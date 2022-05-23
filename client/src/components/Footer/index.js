e91cc51742a88a21129328658fc5931e65264b04
Lawson Vanderpool <lawsonvanderpool@gmail.com>
2022-05-23 14:34:27 -0700

    changed thought to appointment in pages, components and app on client side

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -25,7 +25,7 @@ const Footer = () => {
           >
             ❤️
           </span>{' '}
-          by the Tech Thoughts team.
+          by the Developer Dynasty team.
         </h4>
       </div>
     </footer>
